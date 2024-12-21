7635bd0a9b0901bfde94db7b7af60b3f003e201e
Jhonata Pereira de Andrade <127758461+Jhonata02@users.noreply.github.com>
2024-12-21 10:38:49 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -16,19 +16,19 @@ driver.get("https://web.whatsapp.com/")
 input("Escaneie o QR Code e pressione Enter para continuar...")
 
 # Defina o nome do contato ou número (no formato internacional)
-contato = "+55XXXXXXX"  # Substitua com o número do contato (ex: +55 11 91234-5678)
-mensagem = "Mensagem de teste"
+contato = "+55 11 91234-5678"  # Substitua com o número do contato (ex: +55 11 91234-5678)
+mensagem = "oi"
 
+pesquisa = driver.find_element(By.XPATH, '//div[@contenteditable="true"][@data-tab="3"]')
+pesquisa.click()
+pesquisa.send_keys(contato)  # Pesquisar pelo nome ou número do contato
+pesquisa.send_keys(Keys.ENTER)
 # Enviar a mensagem 10 vezes
 for i in range(100):
     # Buscar o campo de pesquisa e localizar o contato
-    pesquisa = driver.find_element(By.XPATH, '//div[@contenteditable="true"][@data-tab="3"]')
-    pesquisa.click()
-    pesquisa.send_keys(contato)  # Pesquisar pelo nome ou número do contato
-    pesquisa.send_keys(Keys.ENTER)
     
     # Aguardar o carregamento da conversa
-    time.sleep(2)
+    #time.sleep(2)
     
     # Localizar o campo de entrada de mensagem e enviar a mensagem
     caixa_mensagem = driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span/div/div[2]/div[1]/div/div[1]/p')
